3a41847752d72bfb396b6486fbcd9967ee964357
Sandy8432 <sandipjadhav8432@gmail.com>
2026-04-18 15:39:57 +0530

    feat: integrate Tailwind CSS and refactor application structure
    - Updated package.json to include Tailwind CSS, autoprefixer, and postcss.
    - Removed unused favicon.svg and icons.svg files.
    - Deleted App.css and migrated styles to Tailwind CSS.
    - Refactored App.jsx to include a Login component and removed unnecessary code.
    - Created new components for dashboards and task management (AdminDashboard, EmployeeDashboard, AcceptTask, CompleteTask, FailedTask, NewTask).
    - Added AuthContext and TaskContext for state management.
    - Implemented localStorage utility for managing local storage operations.

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const Login = () => {
+    const [email,setEmail]=useState("");
+    const [pass,setPass]= useState("");
+    const handleSubmit = (e)=>{
+         e.preventDefault();
+        
+    }
+  return (
+    <div className="flex h-screen w-screen justify-center items-center bg-black">
+      <div className="border-2 border-emerald-600 p-20 rounded-lg">
+        <form 
+        onSubmit={(e)=>{
+            handleSubmit(e); 
+        }}
+        className="flex flex-col items-center justify-center gap-6"
+        >
+          <input 
+            className=" outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400"
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e)=>{
+            setEmail(e.target.value)
+            }}
+          required
+          />
+          <input 
+            className=" outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400"
+            type="current-password"
+            placeholder="Enter your password"
+            value={pass}
+            onChange={(e)=>{
+            setPass(e.target.value)
+            }}
+          required
+          />
+          <button 
+            className="text-white border-none bg-emerald-600 hover:bg-emerald-700 py-3 px-5 mt-2 rounded-full"
+            type="submit"
+          >
+            Log in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
